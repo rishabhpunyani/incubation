71aa6baa1daa537528473fc53bbd02d0890967cb
rishabhpunyani <rishabharora2403@gmail.com>
2017-08-24 12:16:28 +0530

    final just before small chnges

--- p3/js/script.js
+++ p3/js/script.js
@@ -3,27 +3,25 @@ var ctx = canvas.getContext("2d");
 document.onkeydown = checkKey;
 var canvasWidth = 600;
 var canvasHeight = 600;
-var chickenLenght = canvasWidth / 12;
-var craftLenght = canvasWidth / 6; 
+var chickenLength = canvasWidth / 12;
+var craftLength = canvasWidth / 6; 
 var time = 0 ;
 var y = 0;
 var level = 1;
 var grid = [];
 var moveBullet;
-var myVar;
-var timmer;
-var chicken = new Image();      //  
-chicken.src = './img/chicken.png';  // 
-var sc = new Image(); //
-sc.src = './img/sc.png';//
-var b = new Image();//
-b.src = './img/bullet.png';//
-var bgg = new Image();//--------------------
-bgg.src = './img/bg.jpg'//----------------------
-var st = 0;//
-var score = 0;//
+var LevelTimer;
+var timeLeft;
+var score = 0;
+var GameStatus = "inactive";
 
-var Intro = function(){///////////////////////
+var introtheme = new Audio('./audio/introtheme.ogg');                  //Audio
+var chickenhit = new Audio('./audio/chickenhit.oga');
+var gameover = new Audio('./audio/gameover.oga');
+var maintrack = new Audio('./audio/maintrack.oga');
+var bulletfire = new Audio('./audio/bullet.ogg');
+
+var intro = function(){                                             //Onload function to create intro screen
     ctx.font = "50px Aerial";
     ctx.fillStyle = "white";
     ctx.textAlign = "center";
@@ -32,110 +30,39 @@ var Intro = function(){///////////////////////
     ctx.fillText("Press Enter to Start!", canvas.width/2, canvas.height/2-100);
     ctx.font = "20px Aerial";
     ctx.fillText("Game Controls: Use Arrow keys to control movement and Space to fire", 
-    canvas.width/2, canvas.height/2+200);
+    canvas.width/2, canvas.height/2 + 200);
+    introtheme.play();
 };
 
-var craft = {
-    xPos: canvasWidth / 2 - craftLenght/2,
-    yPos: canvasHeight - canvasHeight / 6 - craftLenght / 2,
-    dim: craftLenght
-}
-
-var icraft = craft;
-
-var bg = function(){
-
-}
-var bullet = {
-    xPos: 0,
-    yPos: 0,
-    dim: craftLenght / 10,
-    status: "active"
-}
-var startGame = function(){////////////////////
-    craft.xPos = canvasWidth / 2 - craftLenght/2;
-    craft.yPos = canvasHeight - canvasHeight / 6 - craftLenght / 2;
-    ctx.clearRect(0, 0, 600, 600);
-    makeGrid();
-    makeGrid();
-    makeCraft();
-    time = 1;
-    clearInterval(myVar);
-    myVar = setInterval(function(){
-        if( y > level + 3){
-            clearInterval(myVar);
+var activeGame = function(){ 
+    craft.xPos = canvasWidth / 2 - craftLength/2;                           //setting initial spacecraft position
+    craft.yPos = canvasHeight - canvasHeight / 6 - craftLength / 2;
+    ctx.clearRect(0, 0, 600, 600);                                          //clearing canvas for game
+    makeGrid(); 
+    makeCraft();        
+    scoreboard();
+    time = (level + 3) * 4 + 2;
+    clearInterval(LevelTimer);
+    LevelTimer = setInterval(function(){                                    //Different level-timer for different levels
+        if( y > level + 2){
             overGame();
-        }
+            clearInterval(LevelTimer);
+            }
         y++;
-        moveGrid();
-    },4000);
-    timmer = setInterval(function(){
-        ctx.clearRect(0, canvasHeight - 50 , 100 , 50);
-        ctx.font = "30px Aerial";
+        moveGrid();                                                         //moving grid downward
+        },4000);
+    timeLeft = setInterval(function(){                                      //time left counter
+        ctx.clearRect(canvas.width-300, canvas.height-40, 150, 40);
+        ctx.font = "25px Aerial";
         ctx.fillStyle = "white";
-        ctx.fillText(time, 0, canvasHeight - 50 );
-        time = time + 1;
-    },1000)
+        ctx.fillText("Timeleft: " + time,canvas.width-240, canvas.height-5);
+        time -=1;
+    },1000);
+    introtheme.pause();
+    maintrack.play();
 };
 
-
-var overGame = function(){
-    clearInterval(myVar);
-    ctx.clearRect(0, 0, 600, 600);
-    st=0;score=0;
-    ctx.font = "50px Aerial";
-    ctx.fillStyle = "white";
-    ctx.textAlign = "center";
-    ctx.fillText("You Lost", canvas.width/2, canvas.height/2-200);
-    ctx.font = "30px Aerial";
-    ctx.fillText("Press Enter to play Again!", canvas.width/2, canvas.height/2-100);
-    ctx.font = "20px Aerial";
-    ctx.fillText("Game Controls: Use Arrow keys to control movement and Space to fire", 
-    canvas.width/2, canvas.height/2+200);
-    y = 0;
-    clearInterval(timmer);
-};
-
-var wonGame = function(){
-    clearInterval(myVar);
-    deleteGrid();
-    ctx.clearRect(0, 0, 600, 600);
-    clearInterval(timmer);
-    ctx.font = "50px Aerial";
-    ctx.fillStyle = "white";
-    ctx.textAlign = "center";
-    ctx.fillText("You Won score: " + score, canvas.width/2, canvas.height/2-200);
-    ctx.font = "30px Aerial";
-    ctx.fillText("Press Enter to play level " + (level + 1) , canvas.width/2, canvas.height/2-100);
-    ctx.font = "20px Aerial";
-    ctx.fillText("Game Controls: Use Arrow keys to control movement and Space to fire", 
-    canvas.width/2, canvas.height/2+200);
-    y = 0;
-    st=0;
-    score=0;
-    if( level > 5){
-        level = 1;
-        overGame();
-    }
-    else{
-        level = level + 1;
-    }
-    
-   
-}
-//
-var drawChicken = function(i, j){///////////////////////////////
-    width = canvasWidth / 12;
-    ctx.drawImage(chicken,grid[i][j].xPos, grid[i][j].yPos, width, width);
-}
-
-var deleteChicken = function(i, j){
-    ctx.fillStyle = "#FF0000";
-    width = canvasWidth / 12;
-    ctx.clearRect(grid[i][j].xPos, grid[i][j].yPos, width, width);
-}
-
-var makeGrid = function(){
+var makeGrid = function(){                                      // function to create each chicken position
     width = canvasWidth / 12;
     for (var i = 0; i < level; i++) {
         grid[i] = [];
@@ -150,7 +77,7 @@ var makeGrid = function(){
     }
 }
 
-var updateGrid = function( ){
+var updateGrid = function( ){                                    //updating grid on bullet hit
     var k = 0; 
     for (var i = level - 1; i >= 0; i--) {
         for (var j = 9; j >= 0; j--) {
@@ -166,14 +93,16 @@ var updateGrid = function( ){
     }
 
 }
-var deleteGrid = function( ){
+
+var deleteGrid = function( ){                                   //deleting full grid
     for (var i = 0; i < level; i++) {
         for (var j = 0; j < 10; j++) {
             deleteChicken(i,j);  
         }
     }
 }
-var moveGrid = function( ){
+
+var moveGrid = function( ){                                     //moving grid after specific interval
     for (var i = 0; i < level; i++) {
         for (var j = 0; j < 10; j++) {
             if(grid[i][j].status == "alive"){
@@ -181,13 +110,11 @@ var moveGrid = function( ){
             }  
         }
     }
-    console.log(grid);
     for (var i = 0; i < level; i++) {
         for (var j = 0; j < 10; j++) {
             grid[i][j].yPos  += 50;  
         }
     }
-    console.log(grid);
     for (var i = level - 1; i >= 0; i--) {
         for (var j = 9; j >= 0; j--) {
             if(grid[i][j].status == "alive"){
@@ -196,28 +123,105 @@ var moveGrid = function( ){
         }
     }
     craftCrash();
-    ctx.clearRect(0, 0, canvasWidth , 20);
 
 }
 
-var makeCraft = function(){////////////////////////////
-    ctx.drawImage(sc, craft.xPos, craft.yPos, craftLenght, craftLenght);
+var chicken = new Image();
+chicken.src = './img/chicken.png';
+var drawChicken = function(i, j){
+    width = canvasWidth / 12;
+    ctx.drawImage(chicken,grid[i][j].xPos, grid[i][j].yPos, width, width);
 }
 
-var craftCrash = function(){
+var deleteChicken = function(i, j){
+    width = canvasWidth / 12;
+    ctx.clearRect(grid[i][j].xPos, grid[i][j].yPos, width, width);
+}
+
+var craft = {
+    xPos: canvasWidth / 2 - craftLength/2,
+    yPos: canvasHeight - canvasHeight / 6 - craftLength / 2,
+    dim: craftLength
+}
+
+var sc = new Image();
+    sc.src = './img/sc.png';
+var makeCraft = function(){
+    ctx.drawImage(sc, craft.xPos, craft.yPos, craftLength, craftLength);
+}
+
+var deleteCraft = function(){
+    ctx.clearRect(craft.xPos - 2, craft.yPos, craft.dim + 5, craft.dim);
+};
+
+var bullet = {
+    xPos: 0,
+    yPos: 0,
+    dim: craftLength / 10,
+    status: "active"
+}
+
+var makeBullet = function(){
+    var b = new Image();
+    b.src = './img/bullet.png';
+    ctx.drawImage( b , bullet.xPos, bullet.yPos, bullet.dim, bullet.dim);
+    bulletfire.play();
+}
+
+var deleteBullet = function(){
+    ctx.clearRect(bullet.xPos, bullet.yPos, bullet.dim , bullet.dim);
+}
+
+var inactiveBullet = function(){                                        //If bullet hit top it will disapper
+    if( bullet.yPos < 10){
+        ctx.clearRect(bullet.xPos, bullet.yPos, bullet.dim , bullet.dim);
+        clearInterval(moveBullet);
+        bullet.status = "active";
+    }
+}
+
+var destroyChicken = function(){                                    //Function for destroying chickensfrom grid
+    var k = 0;
+    for(i = level - 1; i >= 0; i-- ){
+        for(j = 9;j >= 0; j--){
+            var c = grid[i][j];
+            if(c.status == "alive"){
+                if((c.xPos < bullet.xPos + 10 && bullet.xPos < c.xPos + chickenLength) 
+                    && ( bullet.yPos < c.yPos + chickenLength - 10)&& bullet.yPos > c.yPos - 10){
+                    grid[i][j].status = "dead";
+                    console.log(i,j);
+                    deleteBullet();
+                    deleteChicken(i,j);
+                    clearInterval(moveBullet);
+                    bullet.status = "active";
+                    k++;
+                    score += 10;
+                    scoreboard();
+                    chickenhit.play();
+                    break;
+                }
+            }
+        }
+    }
+
+    updateGrid();
+};
+
+
+var craftCrash = function(){                                //Game over if spacecraft crashes into chicken 
     for (var i = level - 1; i >= 0; i--) {
         for (var j = 9; j >= 0; j--) {
             if(grid[i][j].status == "alive"){
-                if( craft.yPos < grid[i][j].yPos + chickenLenght)
+                if( craft.yPos < grid[i][j].yPos + chickenLength)
                 {
                     if (craft.xPos >= grid[i][j].xPos) {
-                        if (craft.xPos - grid[i][j].yPos < chickenLenght) {
+                        if (craft.xPos - grid[i][j].yPos < chickenLength) {
                             overGame();
                             break;
                         }
                     }
                     else{
-                        if(grid[i][j].xPos - craft.xPos < chickenLenght){
+                        if(grid[i][j].xPos - craft.xPos < craft.dim){
                             overGame();
                             break;
                         }
@@ -229,55 +233,67 @@ var craftCrash = function(){
     }
 }
 
-var destroyChicken = function(){
-    var k = 0;
-    for(i = level - 1; i >= 0; i-- ){
-        for(j = 9;j >= 0; j--){
-            var c = grid[i][j];
-            if(c.status == "alive"){
-                if((c.xPos < bullet.xPos + 10 && bullet.xPos < c.xPos + chickenLenght) 
-                    && ( bullet.yPos < c.yPos + chickenLenght - 10)&& bullet.yPos > c.yPos - 10){
-                    grid[i][j].status = "dead";
-                    console.log(i,j);
-                    deleteBullet();
-                    deleteChicken(i,j);
-                    clearInterval(moveBullet);
-                    bullet.status = "active";
-                    k++;
-                    scoreboard();
-                    break;
-                }
-            }
-        }
+var scoreboard = function(){
+    ctx.clearRect(canvas.width-120, canvas.height-40, 120, 40);
+    ctx.font = "25px Aerial";
+    ctx.fillStyle = "white";
+    ctx.fillText("Score:"+ score, canvas.width-80, canvas.height-5);
+};
+
+/*CREATE FUNCTION FOR TIME*/
+
+var wonGame = function(){
+    clearInterval(LevelTimer);
+    ctx.clearRect(0, 0, 600, 600);
+    clearInterval(timeLeft);
+    ctx.font = "45px Aerial";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText("You Won and your score: " + score, canvas.width/2, canvas.height/2-200);
+    ctx.font = "30px Aerial";
+    ctx.fillText("Press Enter to play Level: " + (level + 1) , canvas.width/2, canvas.height/2-100);
+    ctx.font = "20px Aerial";
+    ctx.fillText("Game Controls: Use Arrow keys to control movement and Space to fire", 
+    canvas.width/2, canvas.height/2+200);
+    y = 0;
+    GameStatus = "inactive";
+    maintrack.pause();
+    introtheme.play();
+    if( level > 7){
+        level = 1;
+        overGame();
     }
-
-    updateGrid();
-};
-
-var deleteCraft = function(){
-    ctx.clearRect(craft.xPos - 2, craft.yPos, craft.dim + 5, craft.dim);
-};
-
-var makeBullet = function(){////////////////////////////////
-    ctx.drawImage( b , bullet.xPos, bullet.yPos, bullet.dim, bullet.dim);
-}
-
-var deleteBullet = function(){
-    ctx.clearRect(bullet.xPos, bullet.yPos, bullet.dim , bullet.dim);
-}
-var stopBullet = function(){
-    if( bullet.yPos < 10){
-        clearInterval(moveBullet);
-        bullet.status = "active";
+    else{
+        level = level + 1;
     }
 }
 
+var overGame = function(){
+    deleteGrid();
+    clearInterval(LevelTimer);
+    ctx.clearRect(0, 0, 600, 600);
+    clearInterval(timeLeft);
+    GameStatus = "inactive"; 
+    ctx.font = "40px Aerial";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText("Game Over!", canvas.width/2, canvas.height/2-200);
+    ctx.font = "30px Aerial";
+    ctx.fillText("Press Enter to play Again!", canvas.width/2, canvas.height/2-100);
+    ctx.font = "20px Aerial";
+    ctx.fillText("Game Controls: Use Arrow keys to control movement and Space to fire", 
+    canvas.width/2, canvas.height/2+200);
+    y = 0;
+    score = 0;
+    maintrack.pause();
+    gameover.play();
+};
 
-function checkKey(e) {
 
+function checkKey(e) {                      //Event handling of keypress
     e = e || window.event;
-    if(st==1)/////////////////////////////////////////////
-{
+    if(GameStatus == "active")
+    {
     if (e.keyCode == '32' && bullet.status == "active") {
         // up arrow
 
@@ -289,7 +305,7 @@ function checkKey(e) {
             deleteBullet();
             bullet.yPos = bullet.yPos - 20;
             makeBullet();
-            stopBullet();
+            inactiveBullet();
             destroyChicken();
         }, 25);
     }
@@ -303,7 +319,7 @@ function checkKey(e) {
     }
     else if (e.keyCode == '40') {
         // down arrow
-        if (craft.yPos < canvasHeight - 10 - craft.dim ){
+        if (craft.yPos < canvasHeight - 40 - craft.dim ){
             deleteCraft();
             craft.yPos = craft.yPos + 10; 
             makeCraft();
@@ -328,18 +344,11 @@ function checkKey(e) {
             makeCraft();
             craftCrash();
         }
-    }
-}
-    else if (e.keyCode == 13) {//////////////////////////////////
-        startGame();
-        st+=1;
+     }
+  }
+    else if (e.keyCode == 13) {
+        activeGame();
+        GameStatus = "active";
     }
 }
 
-var scoreboard = function(){/////////////////////////////////////////
-    score+=10;
-    ctx.clearRect(canvas.width-70, canvas.height-20, 40 , 20);
-    ctx.font = "30px Aerial";
-    ctx.fillStyle = "white";
-    ctx.fillText("score:"+ score, canvas.width-70, canvas.height-10);
-};
